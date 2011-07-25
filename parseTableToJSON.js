1f2e7673edf9bc02c15b34c996b02d86594e1db1
Marco Batista <mail@marcobatista.net>
2011-07-25 18:39:59 +0100

    versão alfa, muito alfa
    Converte uma tabela num objecto JSON

---
+++ parseTableToJSON.js
@@ -0,0 +1,12 @@
+// funcção que vai fazer converter uma tabela num objecto JSON
+//Input: Table ID
+//Output: JSON Object
+
+function parseTableToJSON(tableID){
+
+	var table = document.getElementById(tableID);
+	var header = new Array();
+	var content = new Array();
+	
+
+}
